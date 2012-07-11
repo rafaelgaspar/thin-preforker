9d2612a8e10b722feaa5baaa69a0e7520e9037d5
Rafael Gaspar <rafael.gaspar@me.com>
2012-07-11 20:48:21 -0300

    Version bump to 0.0.4

--- lib/thin/preforker/version.rb
+++ lib/thin/preforker/version.rb
@@ -4,7 +4,7 @@ unless defined?(Thin::Preforker::VERSION)
       module VERSION #:nodoc:
         MAJOR = 0
         MINOR = 0
-        TINY = 3
+        TINY = 4
     
         STRING = [MAJOR, MINOR, TINY].join('.')
       end
